a45226e4c7cb624d89e22f375e5a3369a4aa3989
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2021-01-24 20:47:48 -0500

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,49 @@
 // Add BandInput component
 import React, { Component } from 'react'
 
+
 class BandInput extends Component {
+  constructor(){
+    super();
+    this.state = {
+      name: '',
+    }
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    this.props.addBand(this.state)
+    this.setState({
+      name: '',
+    })
+  }
+
+  handleChange(event) {
+    this.setState({
+      name: event.target.value
+    });
+  }
+
+
   render() {
     return(
       <div>
-        Band Input
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <div>
+            <input type="text" onChange={(event) => this.handleChange(event)} placeholder="Band Name" value={this.state.name} />
+          </div>
+          <div>
+            <input type="submit" />
+          </div>
+        </form>
       </div>
     )
   }
 }
 
-export default BandInput
+// const mapDispatchToProps = dispatch => ({
+//   addBand: formData => dispatch({type: 'ADD_BAND', payload: formData })
+// })
+
+// export default connect(null, mapDispatchToProps)(BandInput);
+export default BandInput;
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import BandInput from '../components/BandInput'
+
+
 
 class BandsContainer extends Component {
+
+  renderBands = () => this.props.bands.map((band, id) =>
+  <li key={id}>{band.name}</li>)
+
   render() {
     return(
       <div>
+        <BandInput addBand={this.props.addBand} />
+          {this.renderBands()}
         BandsContainer
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapDispatchToProps = dispatch => {
+  return {addBand: band => dispatch({type: 'ADD_BAND', band})}
+}
+
+const mapStateToProps = state => {
+  return {
+    bands: state.bands
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer);
